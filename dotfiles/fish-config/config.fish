f28620e11da9ac6f3d87fe0113e7fd0c3577d229
Mike Botazzo <mikebotazzo@gmail.com>
2015-11-12 18:57:45 +0100

    desfazendo a cagada do diretorio

--- dotfiles/fish-config/config.fish
+++
@@ -1,4 +0,0 @@
-. "$HOME/.config/fish/functions/z.fish"
-. "$HOME/.config/fish/functions/fish_prompt.fish"
-. "$HOME/.config/fish/functions/functions.fish"
-. "$HOME/.config/fish/conda.fish"
--- dotfiles/fish-config/functions/functions.fish
+++
@@ -1,6 +0,0 @@
-function fish_user_key_bindings
-    if status --is-login
-     bind \x1b forward-word
-     bind \x1d  backward-word
-    end
-end
