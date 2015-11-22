29264bd78355d7614b581190c903e5b1cf751d8f
Mike Botazzo <mikebotazzo@users.noreply.github.com>
2015-11-22 23:29:55 +0100

    comment function

--- fish-config/functions/functions.fish
+++ fish-config/functions/functions.fish
@@ -1,3 +1,5 @@
+#Add the `ctrl` + `[` and `ctrl` + `]` key bindings to 
+# move backward and forward respectively between words in command line text
 function fish_user_key_bindings
     if status --is-login
      bind \x1d  forward-word
