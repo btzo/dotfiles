8fe7afc524c7f2cdf01c8c85ce2f614476dd47c0
Mike Botazzo <mikebotazzo@users.noreply.github.com>
2016-12-07 15:02:32 +0100

    add iterm2 integration and git alias

--- fish-config/config.fish
+++ fish-config/config.fish
@@ -2,3 +2,15 @@
 . "$HOME/.config/fish/functions/fish_prompt.fish"
 . "$HOME/.config/fish/functions/functions.fish"
 . "$HOME/.config/fish/functions/updatevim.fish"
+
+eval (python -m virtualfish compat_aliases)
+
+#Add alias for git
+alias st='git status'
+alias di='git diff'
+alias push='git push'
+alias pull='git pull'
+alias remote='git remote'
+alias map='gitup map'
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
