634f0d5b9babc7b12b41726774e92aab7b932243
Mike Botazzo <mikebotazzo@users.noreply.github.com>
2016-12-07 09:15:50 +0100

    remove unnecessary function

--- fish-config/config.fish
+++ fish-config/config.fish
@@ -1,4 +1,4 @@
 . "$HOME/.config/fish/functions/z.fish"
 . "$HOME/.config/fish/functions/fish_prompt.fish"
 . "$HOME/.config/fish/functions/functions.fish"
-. "$HOME/.config/fish/conda.fish"
+. "$HOME/.config/fish/functions/updatevim.fish"
